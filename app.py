8dcd1c0d05df17b5a4ce350a7edd1b360e4cbaa5
Gagan-AIML <khajuriagagan29@gmail.com>
2026-04-06 13:30:27 +0530

    updated requriments.txt and app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGener
 from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 # --- 1. CONFIGURATION & SETUP ---
